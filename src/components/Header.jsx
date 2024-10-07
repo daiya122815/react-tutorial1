b91882b10c3e962fa8453adb6a47d0e8dd2ac258
daiya122815 <daiya122815@gmail.com>
2024-10-07 21:56:02 +0900

    Main.jsxのコンポーネント分割

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,11 @@
+import Nav from "./Nav";
+
 export default function Header() {
   return (
     <header>
       <h1>Birdwatching</h1>
       <img src="/images/dove.png" alt="a simple dove logo" />
-
-      <nav>
-        <ul>
-          <li><span>Home</span></li>
-          <li><a href="#">Get started</a></li>
-          <li><a href="#">Photos</a></li>
-          <li><a href="#">Gear</a></li>
-          <li><a href="#">Forum</a></li>
-        </ul>
-      </nav>
-
+      <Nav />
     </header>
   );
 }
